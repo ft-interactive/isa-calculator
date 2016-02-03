e05a7883c11a556604d6721ecf0aa412b3c3f050
Bob Haslett <bob.haslett@ft.com>
2016-02-03 17:28:16 +0000

    function to add slider labels and make visible

--- client/scripts/main.js
+++ client/scripts/main.js
@@ -10,11 +10,36 @@ document.addEventListener('DOMContentLoaded', () => {
 	// remove the 300ms tap delay on mobile browsers
 	attachFastClick(document.body);
 
+	var savePerYear=1
+	var timeToRetire=1
+	var charges=0
+	var returns=0
+
+	var slideValues=[{"divID":"#save","labName":"savelab","pos":1,"sliderID":"#slsave"},
+	{"divID":"#toRetire","labName":"retirelab","pos":1,"sliderID":"#slretire"},
+	{"divID":"#charges","labName":"chargeslab","pos":0,"sliderID":"#slcharges"},
+	{"divID":"#returns","labName":"returnslab","pos":0,"sliderID":"#slreturns"}]
+
+	//Add labels to the sliders then make them visible
+	for (var i = 0; i < slideValues.length; i++) { 
+		var div=slideValues[i].divID;
+		var lab=slideValues[i].labName;
+		var pos=Number(slideValues[i].pos)
+		addLabel(div,lab);
+		var newX=calcLabelPos(slideValues[i].pos,slideValues[i].sliderID);
+		moveLabel("#"+lab,pos,newX);
+
+	}
+
 	//Add labels to sliders
-	addLabel("#save","savelab");
-	addLabel("#toRetire","retirelab");
-	addLabel("#charges","chargeslab");
-	addLabel("#returns","returnslab");
+	// addLabel("#save","savelab");
+	// // newX=calcLabelPos(savePerYear,"#slsave")
+	// // moveLabel("#savelab",savePerYear,newX);
+	// addLabel("#toRetire","retirelab");
+	// addLabel("#charges","chargeslab");
+	// addLabel("#returns","returnslab");
+
+
 
 
 
@@ -46,7 +71,6 @@ document.addEventListener('DOMContentLoaded', () => {
 	var returnsevent=d3.select("#slreturns");
 	returnsevent.on("change", function(d){
 		var interval=this.value;
-		console.log(interval);
 		var newX=calcLabelPos(interval,"#slreturns")
 		moveLabel("#returnslab",interval,newX);
 	})
@@ -59,18 +83,20 @@ document.addEventListener('DOMContentLoaded', () => {
 		.attr("class", "slideLabel");
 	}
 
-	function moveLabel(divId,labelVal,pos){
+	function moveLabel(divId,labelText,pos){
 		var label=d3.select(String(divId))
-		.html(labelVal)
+		.html(labelText)
 		.style("left", pos+"px")
 		.style("top", "54px");
 	}
 
-	function calcLabelPos (pos, label){
-		var slider=d3.select(String(label))
+	function calcLabelPos (pos, SliderID){
+		console.log(SliderID)
+		var slider=d3.select(String(SliderID))
 		var percentage=(100/(slider.node().max-slider.node().min)*(pos));
 		var posX=slider.node().getBoundingClientRect().width;
 		posX=(posX/100)*percentage;
+		console.log(posX)
 		return posX
 	}
 
